9e05542d43804b2a76e4f018fc47d187d220f2e8
Victor Westerlund <victor.vesterlund@gmail.com>
2021-01-29 13:52:24 +0100

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+async function fetchManifest(manifest) {
+	const response = await fetch(`manifest/${manifest}.json`);
+	return response.json();
+}
+
+class ProductFilter {
+
+	constructor(input,results) {
+		this.imageSize = 90;
+
+		this.results = results;
+		this.manifest = {};
+
+		input.addEventListener("keyup",event => this.search(event));
+	}
+
+	setManifest(manifest) {
+		this.manifest = manifest;
+		this.restoreTree();
+	}
+
+	restoreTree(manifest = this.manifest) {
+		for(const [key,value] of Object.entries(manifest)) {
+			this.append(key,value);
+		}
+	}
+
+	// Clear result subtree
+	resetTree() {
+		while(this.results.firstChild) {
+			this.results.removeChild(this.results.lastChild);
+		}
+	}
+
+	// Append item to view
+	append(id,url) {
+		const HTML = `
+		<div class="item">
+			<img src="https://www.deltaco.se/sites/cdn/PublishingImages/Products/${id}.png?width=${this.imageSize}"/>
+			<h2>${id}</h2>
+			<a href="${url}" target="_blank"><p class="button">Download</p></a>
+		</div>`
+		;
+
+		this.results.insertAdjacentHTML("beforeend",HTML);
+	}
+
+	// Filter view with string from input
+	search(event) {
+		const input = event.target.value.toUpperCase();
+
+		this.resetTree();
+
+		for(const [key,value] of Object.entries(this.manifest)) {
+			if(key.includes(input)) {
+				this.append(key,value);
+			}
+		}
+	}
+
+}
+
+/* ---- */
+
+const manifest = new URLSearchParams(window.location.search).get("manifest");
+const elements = {
+	input: document.getElementsByTagName("input")[0],
+	results: document.getElementById("results")
+}
+
+// Create new pr
+const search = new ProductFilter(elements.input,elements.results);
+
+// Load manifest
+fetchManifest(manifest).then(manifest => search.setManifest(manifest));
